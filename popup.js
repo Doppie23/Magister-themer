dfa8b005625babd2348dfa6d97a71c9f494db243
Doppie22 <dvopstal05@gmail.com>
2022-10-11 22:10:35 +0200

    commit

--- popup.js
+++ popup.js
@@ -6,6 +6,12 @@ function watchHoofdkleur(event) {
     document.querySelectorAll("h1").forEach((p) => {
       p.style.color = event.target.value;
       console.log(event.target.value)
+
+
+      /*
+      event target opslaan in chrome storage en daar dan naar luisteren in script.js
+      kan dan ook weer ophalen bij reload en dus dan bij verandering
+      */
     });
   }
 
