3b426ed7d9e7c7eb46c66bd0d4d5d16e97a15e24
Doppie22 <dvopstal05@gmail.com>
2022-10-14 19:52:20 +0200

    beter hash

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 addEventListener('hashchange', (event) => {
-    console.log(event.newURL)
+    console.log(event.currentTarget.location.hash)
 
 });
 
 window.onload = function(event) {
-    console.log(event.currentTarget.location.href)
+    console.log(event.currentTarget.location.hash)
     
 };
+
+function mainpage(hoofdkleur, seckleur, titel, tekst){
+    
+}
