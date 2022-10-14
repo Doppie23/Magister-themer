669dc2974fc7df5dc5c0a48e8d9c07a104312ee3
Doppie22 <dvopstal05@gmail.com>
2022-10-15 00:36:06 +0200

    coomit voot fout

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+const hoofdkleur = chrome.storage.sync.get('hoofdkleur')
+const seckleur = chrome.storage.sync.get('seckleur')
+const titel = chrome.storage.sync.get('titel')
+const tekst = chrome.storage.sync.get('tekst')
+
+
 addEventListener('hashchange', (event) => {
     console.log(event.currentTarget.location.hash)
 
     if(event.currentTarget.location.hash === "#/vandaag"){
-        console.log('epicly')
+        mainpage();
     }
 
 });
