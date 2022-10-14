d4c9aee39cbaa3d125959b87d20e44db68241a4b
Doppie22 <dvopstal05@gmail.com>
2022-10-14 19:42:26 +0200

    inlog moet ngo als knop klikt

--- inlog.js
+++ inlog.js
@@ -18,10 +18,48 @@ chrome.storage.onChanged.addListener(function(changes) {
 
     document.getElementsByClassName('dna-btn-primary')[0].style.backgroundColor = titel.newValue;
 });
+
 //tekst
 chrome.storage.onChanged.addListener(function(changes) {
   var tekst = changes['tekst'];
   console.log(tekst.newValue);
-  document.getElementsByTagName('h2')[0].style.color = tekst.newValue
+  document.getElementsByTagName('h2')[0].style.color = tekst.newValue;
+  document.getElementsByClassName('dna-btn-primary')[0].style.color = tekst.newValue;
 });
   
+waitForElm('.challenge-actions').then(() => {
+  chrome.storage.sync.get(['hoofdkleur'], function(hoofdkleur) {
+    document.body.style.backgroundColor = hoofdkleur.hoofdkleur;
+  });
+
+  chrome.storage.sync.get(['titel'], function(titel) {
+    document.getElementsByTagName('h1')[0].style.color = titel.titel;
+    document.getElementsByTagName('path')[1].style.fill = titel.titel;
+    document.getElementsByClassName('dna-btn-primary')[0].style.backgroundColor = titel.titel;
+  });
+  
+  chrome.storage.sync.get(['tekst'], function(tekst) {
+    document.getElementsByTagName('h2')[0].style.color = tekst.tekst;
+    document.getElementsByClassName('dna-btn-primary')[0].style.color = tekst.tekst;
+  });
+});
+
+function waitForElm(selector) {
+  return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+          return resolve(document.querySelector(selector));
+      }
+
+      const observer = new MutationObserver(mutations => {
+          if (document.querySelector(selector)) {
+              resolve(document.querySelector(selector));
+              observer.disconnect();
+          }
+      });
+
+      observer.observe(document.body, {
+          childList: true,
+          subtree: true
+      });
+  });
+}
--- popup.js
+++ popup.js
@@ -29,3 +29,23 @@ kan dan ook weer ophalen bij reload en dus dan bij verandering
 function watchTitel(event) {chrome.storage.sync.set({ titel: event.target.value })}
 function watchTekst(event) {chrome.storage.sync.set({ tekst: event.target.value })}
 
+document.addEventListener('DOMContentLoaded', restore_options);
+
+function restore_options() {
+  chrome.storage.sync.get(['hoofdkleur'], function(hoofdkleur) {
+    document.getElementById('hoofdkleur').value = hoofdkleur.hoofdkleur
+  });
+
+  // chrome.storage.sync.get(['seckleur'], function(seckleur) {
+    // document.getElementById('seckleur').value = seckleur.seckleur
+  // });
+  
+  chrome.storage.sync.get(['titel'], function(titel) {
+    document.getElementById('titel').value = titel.titel
+  });
+
+  chrome.storage.sync.get(['tekst'], function(tekst) {
+    document.getElementById('tekst').value = tekst.tekst
+  });
+
+  }
