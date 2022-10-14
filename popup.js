168bed113af7e71ccc2afdd966d376d64607276f
Doppie22 <dvopstal05@gmail.com>
2022-10-14 19:47:06 +0200

    betere syntax

--- popup.js
+++ popup.js
@@ -2,30 +2,17 @@
 var hoofdkleur = document.getElementById("hoofdkleur");
 hoofdkleur.addEventListener("change", watchHoofdkleur, false);
 
+var seckleur = document.getElementById("seckleur");
+seckleur.addEventListener("change", watchSeckleur, false);
+
 var titel = document.getElementById("titel");
 titel.addEventListener("change", watchTitel, false);
 
 var tekst = document.getElementById("tekst");
 tekst.addEventListener("change", watchTekst, false);
 
-
-function watchHoofdkleur(event) {
-  console.log(event.target.value)
-  chrome.storage.sync.set({ hoofdkleur: event.target.value })
-
-
-/*
-event target opslaan in chrome storage en daar dan naar luisteren in script.js
-kan dan ook weer ophalen bij reload en dus dan bij verandering
-*/
-
-}
-
-// chrome.storage.onChanged.addListener(function(changes) {
-//   var action = changes['hoofdkleur'];
-//   console.log(action.newValue)
-// });
-
+function watchHoofdkleur(event) {chrome.storage.sync.set({ hoofdkleur: event.target.value })}
+function watchSeckleur(event) {chrome.storage.sync.set({ seckleur: event.target.value })}
 function watchTitel(event) {chrome.storage.sync.set({ titel: event.target.value })}
 function watchTekst(event) {chrome.storage.sync.set({ tekst: event.target.value })}
 
@@ -36,9 +23,9 @@ function restore_options() {
     document.getElementById('hoofdkleur').value = hoofdkleur.hoofdkleur
   });
 
-  // chrome.storage.sync.get(['seckleur'], function(seckleur) {
-    // document.getElementById('seckleur').value = seckleur.seckleur
-  // });
+  chrome.storage.sync.get(['seckleur'], function(seckleur) {
+    document.getElementById('seckleur').value = seckleur.seckleur
+  });
   
   chrome.storage.sync.get(['titel'], function(titel) {
     document.getElementById('titel').value = titel.titel
