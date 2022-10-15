7f419702c22d3787649d61c43df7a8112d82ed50
Doppie22 <dvopstal05@gmail.com>
2022-10-15 15:19:19 +0200

    verder

--- script.js
+++ script.js
@@ -8,21 +8,30 @@ addEventListener('hashchange', (event) => {
 
     if(event.currentTarget.location.hash === "#/vandaag"){
         chrome.storage.sync.get(['hoofdkleur'], function(hoofdkleur) {
+            //bg en balk
             document.body.style.backgroundColor = hoofdkleur.hoofdkleur;
             document.documentElement.style.setProperty('--secondary-background', hoofdkleur.hoofdkleur);
             document.getElementsByClassName('menu-footer')[0].style.backgroundColor = hoofdkleur.hoofdkleur;
         });
 
         chrome.storage.sync.get(['seckleur'], function(seckleur) {
+            //balk
             document.documentElement.style.setProperty('--primary-background', seckleur.seckleur);
             document.documentElement.style.setProperty('--background-1', seckleur.seckleur);
         });
 
         chrome.storage.sync.get(['titel'], function(titel) {
-            waitForElm('.container').then(() => {
-                // document.getElementsByClassName('title')[0].style.color = titel.titel;
-                var x = document.getElementsByClassName('container')
-                console.log(x.getElementsByClassName('title'))
+            waitForElm('.title').then(() => {
+                //dag titel
+                var x = document.getElementsByClassName('view')
+                var x = x[0].getElementsByClassName('container')[0].getElementsByTagName('dna-page-header')[0].shadowRoot;
+                x.querySelector(".container").getElementsByClassName('title')[0].style.color = titel.titel;
+
+                //pas schermindelding aan
+                var x = document.getElementsByClassName('view')
+                var x = x[0].getElementsByClassName('container')[0].getElementsByTagName('dna-page-header')[0];
+                x.getElementsByTagName('dna-button-bar')[0].getElementsByTagName('dna-button')[0].style.color = titel.titel;
+                x.getElementsByTagName('dna-button-bar')[0].getElementsByTagName('dna-button')[0].style.borderColor = titel.titel;
             });
             
         });
